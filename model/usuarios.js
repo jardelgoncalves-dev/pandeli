8e7dd557d328a2f403a44221455ae97bb730448f
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-15 18:17:22 -0300

    esquema de tabela Usuarios

---
+++ model/usuarios.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Usuarios = new Schema({
+    nome:String,
+    sobrenome:String,
+    email:String,
+    password:String,
+    cpf:String,
+    endereco:{
+        rua:String,
+        numero:Number,
+        bairro:String,
+        cidade:String,
+        uf:String,
+        cep:String
+    },
+    compras:[],
+    nivel_acesso:Number
+},{
+    versionKey: false
+})
+
+module.exports = mongoose.model("Usuarios", Usuarios);
