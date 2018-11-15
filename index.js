992e3de98eb97e7dfecf16d49b26c69bbaa3e14b
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-15 18:06:40 -0300

    configurações do server

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+// Configuração e Conexão com o mongodb
+mongoose.set('debug', true);
+mongoose.connect("mongodb://localhost:2702/pandeli-db", { useNewUrlParser: true })
+    .then(function(){
+        console.log("Banco de dados Conectado")
+    })
+    .catch(function(){
+        console.error(err)
+    });
+
+// Configurações
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/public/views");
+app.use(express.static(__dirname + "/public"));
+app.set("port", 3000)
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(app.get("port"), function(){
+    console.log("Server on port", app.get("port"))
+});
