3ec6ab0698b68abcd66d8891d8e9dc49fda617c7
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-15 18:17:13 -0300

    esquema de tabela Produtos

---
+++ model/produtos.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Produtos = new Schema({
+    nome:String,
+    preco:Number,
+    unidade:String,
+    ingredientes:String
+},{
+    versionKey: false
+})
+
+module.exports = mongoose.model("Produtos", Produtos);
