ed1afa82a08a4b0f42205a26bfe9aa3f6401ea62
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-16 01:48:59 -0300

    rota para autenticação

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const controller_login = require("../controllers/login");
 
-router.get("/", controller_login);
+router.get("/", controller_login.login);
+router.post("/auth",controller_login.auth);
 
 
 module.exports = router;
