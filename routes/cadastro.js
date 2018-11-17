0951989aa481a8b3bcfc2f0af08a67664999d17d
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-17 01:33:30 -0300

    add nova rota

---
+++ routes/cadastro.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const controller_cadastro = require("../controllers/cadastro");
+
+router.get("/", controller_cadastro.get);
+router.post("/", controller_cadastro.post);
+
+module.exports = router;
