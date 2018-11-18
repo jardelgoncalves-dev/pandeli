7888905a73667408b1bee486dc89cfee607f5fbf
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-17 23:59:08 -0300

    add functions a serem implementadas

---
+++ controllers/api/produto.js
@@ -0,0 +1,19 @@
+module.exports.addProduto = function(req, res){
+    // dado um produto (recebido pelo req.body), salva na base de dados
+}
+
+module.exports.getAllProdutos = function(req, res){
+    // retorna todos os produtos da base de dados
+}
+
+module.exports.getProduto = function(req, res){
+    // dado um id, retorna um produto
+}
+
+module.exports.updateProduto = function(req, res){
+    // dado um id, atualiza informações do produto
+}
+
+module.exports.deleteProduto = function(req, res){
+    // dado um id, remove o produto
+}
