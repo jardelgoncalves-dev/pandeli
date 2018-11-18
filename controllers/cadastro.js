dc48c1cfe7d71eb4a80fe8576a578f1d3627e78f
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-17 21:56:32 -0300

    alterações no cadastro de usuario

--- controllers/cadastro.js
+++ controllers/cadastro.js
@@ -1,31 +1,58 @@
 const fetch = require('node-fetch');
 module.exports.get = function(req, res){
-    var erros = []
     if(req.session.autorizado){
         req.session.destroy();     
     }
-    res.render("account/cadastro", {erros:erros})
+    let estados = [
+        "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", 
+        "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", 
+        "RO", "RR", "SC", "SP", "SE", "TO"
+    ]
+    res.render("account/cadastro", {erros:req.flash("error"), estados:estados})
 }
 
 module.exports.post = function(req, res){
-    fetch("http://localhost:3000/api/usuario", {
-        method:"POST",
-        body:JSON.stringify(req.body),
-        headers:{
-            "Accept":"application/json",
-            "Content-type":"application/json"
+    if (req.body.password !== req.body.passwordConfirm){
+        req.flash('error', "Senhas não equivalentes.")
+        res.redirect("/cadastro")
+
+    } else {
+        const usuario = {
+            nome:req.body.nome,
+            sobrenome:req.body.sobrenome,
+            email:req.body.email,
+            password:req.body.password,
+            cpf:req.body.cpf,
+            endereco:{
+                rua:req.body.rua,
+                numero:req.body.numero,
+                bairro:req.body.bairro,
+                cidade:req.body.cidade,
+                uf:req.body.uf,
+                cep:req.body.cep
+            },
+            nivel_Acesso:1
         }
-    })
-    .then(function(res) {
-        return res.json()
-    })
-    .then(function(data){
-        if(data.status){
-            req.session.autorizado = true
-            req.session.usuario = data.usuario
-            res.redirect("/dashboard")
-        }else{
-            res.render("account/cadastro", {erros:data.mensage})
-        }
-    })
+        fetch("http://localhost:3000/api/usuario", {
+            method:"POST",
+            body:JSON.stringify(usuario),
+            headers:{
+                "Accept":"application/json",
+                "Content-type":"application/json"
+            }
+        })
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data){
+            if(data.stored){
+                req.session.autorizado = true
+                req.session.usuario = data.usuario
+                res.redirect("/dashboard")
+            }else{
+                req.flash('error', data.message)
+                res.redirect("/cadastro")
+            }
+        })
+    }
 }
