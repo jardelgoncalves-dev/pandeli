ce235d43cc3db3370a39d235633e442076835eb2
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-19 03:10:30 -0300

    novas funcionalidades

--- routes/api/produto.js
+++ routes/api/produto.js
@@ -5,5 +5,6 @@ const controller_produto_api = require("../../controllers/api/produto")
 
 router.get("/", controller_produto_api.getAllProdutos)
 router.post("/", controller_produto_api.addProduto)
+router.delete("/:id", controller_produto_api.deleteProduto)
 
 module.exports = router;
