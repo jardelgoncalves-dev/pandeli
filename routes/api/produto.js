15a4e80c8cb5596b0271c467492aea87b0d0540a
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-19 19:56:10 -0300

    atualiações de rotas

--- routes/api/produto.js
+++ routes/api/produto.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const controller_produto_api = require("../../controllers/api/produto")
 
 router.get("/", controller_produto_api.getAllProdutos)
+router.get("/:id", controller_produto_api.getProduto)
 router.post("/", controller_produto_api.addProduto)
+router.put("/:id", controller_produto_api.updateProduto)
 router.delete("/:id", controller_produto_api.deleteProduto)
 
 module.exports = router;
