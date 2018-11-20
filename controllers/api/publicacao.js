3ea3d78bef4c535f32f69ede2fbbad87e0cc46ba
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-20 00:30:01 -0300

    metodos criados e funcionando

--- controllers/api/publicacao.js
+++ controllers/api/publicacao.js
@@ -1,27 +1,57 @@
+const Publicacao = require("../../model/publicacao");
+const date = require('date-and-time');
+
 module.exports.getPub = function(req, res){
-    // dado um ir, retorna a publicacao
+    const publicacao = Publicacao.findById(req.params.id)
+    .then(function(publicacao){
+        res.status(200).json(publicacao)
+    })
 }
 
 module.exports.getAllPub = function(req, res){
-    // retorna todas as publicações da base de dados
+    Publicacao.find()
+    .then(function(publicacoes){
+        res.status(200).json(publicacoes)
+    })
 }
 
 module.exports.addPub = function(req, res){
-    // dado um objeto (req.body), sava o objeto na sabe de dados e a imagem em um diretório
-    /**
-     * ESQUEMA/MODEL da requisição
-     * {
-     *      titulo: String,
-     *      conteudo: String,
-     *      filename: String
-     * }
-     */
+    let now = new Date();
+    const pub = {
+        titulo:req.body.titulo,
+        conteudo:req.body.conteudo,
+        foto:req.body.foto,
+        data:date.format(now, 'YYYY/MM/DD HH:mm:ss')
+    }
+    const publicacao = new Publicacao(pub)
+    publicacao.save()
+    res.status(200).json({
+        message:"Publicação cadastrada com sucesso"
+    })
 }
 
 module.exports.updatePub = function(req, res){
-    // dado um id, atualiza os dados da publicação (recebido pelo req.body)
+    Publicacao.updateOne({_id:req.params.id},req.body, function(err, result){
+        if(err){
+            res.status(404).json({
+                message:"Ocorreu um erro ao tentar atualizar a publicação"
+            })
+        }
+        res.status(200).json({
+            message:"Atualização realizada com sucesso"
+        })
+    })
 }
 
-module.exports.delete = function(req, res){
-    // dado um id, remove a publicação
+module.exports.deletePub = function(req, res){
+    Publicacao.deleteOne({_id:req.params.id}, function(err, result){
+        if(err){
+            res.status(404).json({
+                message:"Erro ao tentar remover publicação"
+            })
+        }
+        res.status(200).json({
+            message:"Publicação deletada com sucesso"
+        })
+    })
 }
