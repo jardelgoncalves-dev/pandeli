3f1d9448ae1737192f119f46b60999cc69b39a03
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-11-22 03:18:29 -0300

    finalizada

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -2,6 +2,7 @@ module.exports.getPage = function(req, res){
     if(req.session.autorizado){
         res.render("usuario/produtos", {usuario:req.session.usuario})
     } else{
+        req.flash('error', 'É necessário efetuar o login!')
         res.redirect("/login")
     }
 }
